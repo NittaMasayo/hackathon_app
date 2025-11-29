68f5b36688261962187c2558af3420ae6a2d8340
youhei kamimura <kyami0628@gmail.com>
2025-11-30 02:58:57 +0900

    上下ジャイロ１回のバイブレーションに修正

--- lib/presentation/view_model/game/shake_manager.dart
+++ lib/presentation/view_model/game/shake_manager.dart
@@ -12,16 +12,15 @@ class ShakeManager {
   final double _threshold = 2.0;
   StreamSubscription? _subscription;
   bool _isStarted = false;
+  bool _vibrated = false;
 
   ShakeManager({required this.controller});
   final HeartbeatWaveController controller;
 
   void gameStart() {
-    // 既に開始している場合は何もしない
     if (_isStarted) return;
     _isStarted = true;
 
-    // ignore: deprecated_member_use
     _subscription = accelerometerEvents.listen((event) async {
       double z = event.z;
 
@@ -29,29 +28,27 @@ class ShakeManager {
       if (!_movingDown && z < -_threshold) {
         _movingDown = true;
 
-        // 下方向に動いたときだけバイブ
-        if (await Vibration.hasVibrator()) {
-          // 短く強く震える
-          await Vibration.vibrate(
-            pattern: [0, 120, 60, 120],
-            intensities: [255, 255],
-          );
+        // 下方向に動いたときだけ1回バイブ
+        if (!_vibrated && await Vibration.hasVibrator()) {
+          _vibrated = true; // 一度振動したらフラグ立てる
+          await Vibration.vibrate(duration: 50);
           controller.triggerPulse();
         }
       }
 
-      // 上方向に戻ったらカウント
+      // 上方向に戻ったらリセット
       if (_movingDown && z > _threshold) {
         _movingDown = false;
-        controller.triggerPulse();
+        _vibrated = false; // 上方向に戻ったら再び振動可能
       }
     });
   }
 
-  /// リソースを解放
   Future<void> dispose() async {
     await _subscription?.cancel();
     _subscription = null;
     _isStarted = false;
+    _movingDown = false;
+    _vibrated = false;
   }
 }
